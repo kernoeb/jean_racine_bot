ce28f33b2b7bc3aece2af16b579ce0a82dad1768
kernoeb <kernoeb@protonmail.com>
2021-08-15 14:32:12 +0200

    Update register_slash_commands.js

---
+++ register_slash_commands.js
@@ -0,0 +1,34 @@
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const dotenv = require('dotenv')
+const fs = require('fs')
+const logger = require('./utils/signale')
+
+dotenv.config()
+
+const commands = []
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`)
+	commands.push(command.data.toJSON())
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+(async () => {
+	try {
+		logger.info('Started refreshing application (/) commands.')
+
+		await rest.put(
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, 'XXXXX'),
+			// Routes.applicationCommands(process.env.CLIENT_ID),
+			{ body: commands }
+		)
+
+		logger.success('Successfully reloaded application (/) commands.')
+		process.exit()
+	} catch (error) {
+		logger.error(error)
+	}
+})()
