1a7b41fbad2f5788d9e8d86f9dd3f236f00c00ae
kernoeb <kernoeb@protonmail.com>
2021-09-13 19:30:05 +0200

    refactor concurrency usage

--- utils/axios.js
+++ utils/axios.js
@@ -2,10 +2,7 @@ const axios = require('axios')
 const logger = require('../utils/signale')
 const http = require('http')
 const https = require('https')
-
-const MAX_REQUESTS_COUNT = 1
-const INTERVAL_MS = 300
-let PENDING_REQUESTS = 0
+const { ConcurrencyManager } = require('./concurrency')
 
 logger.log('Axios instantiated')
 
@@ -18,28 +15,7 @@ const instance = axios.create({
   httpsAgent: new https.Agent({ keepAlive: true })
 })
 
-instance.interceptors.request.use(function(config) {
-  return new Promise((resolve) => {
-    const interval = setInterval(() => {
-      if (PENDING_REQUESTS < MAX_REQUESTS_COUNT) {
-        PENDING_REQUESTS++
-        clearInterval(interval)
-        resolve(config)
-      }
-    }, INTERVAL_MS)
-  })
-})
-
-/**
- * Axios Response Interceptor
- */
-instance.interceptors.response.use(function(response) {
-  PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1)
-  return Promise.resolve(response)
-}, function(error) {
-  PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1)
-  return Promise.reject(error)
-})
+ConcurrencyManager(instance)
 
 module.exports = instance
 
