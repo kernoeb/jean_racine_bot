bbbfe4c238090be190da56e41e682278426d855f
kernoeb <kernoeb@protonmail.com>
2021-09-21 21:54:34 +0200

    minor fix for profile picture

--- utils/get_profile_picture.js
+++ utils/get_profile_picture.js
@@ -3,17 +3,17 @@ module.exports = {
   async getProfilePicture(id) {
     try {
       const url = `${process.env.ROOTME_URL}/IMG/auton${id}.jpg`
-      await axios.get(url)
+      await axios.get(url, { params: { fakeHash: new Date().getTime() } })
       return url
     } catch (err) {}
     try {
       const url = `${process.env.ROOTME_URL}/IMG/auton${id}.png`
-      await axios.get(url)
+      await axios.get(url, { params: { fakeHash: new Date().getTime() } })
       return url
     } catch (err) {}
     try {
       const url = `${process.env.ROOTME_URL}/IMG/auton${id}.gif`
-      await axios.get(url)
+      await axios.get(url, { params: { fakeHash: new Date().getTime() } })
       return url
     } catch (err) {}
     return null
