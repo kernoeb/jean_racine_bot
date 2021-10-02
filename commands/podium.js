de6e4d297771d3bf94df6f2781618ec69becc624
kernoeb <kernoeb@protonmail.com>
2021-10-03 01:04:26 +0200

    fix: catch errors and env url

--- commands/podium.js
+++ commands/podium.js
@@ -13,13 +13,25 @@ module.exports = {
     .setName('podium')
     .setDescription('Podium sous forme d\'image'),
   async execute(interaction) {
-    const channel = await mongoose.models.channels.findOne({ guildId: interaction.guildId })
-    const tmpUsers = await mongoose.models.user.find({ id_auteur: { $in: (channel.users || []) } }).sort({ score: -1, nom: 1 }).limit(3)
+    try {
+      const channel = await mongoose.models.channels.findOne({ guildId: interaction.guildId })
+      const tmpUsers = await mongoose.models.user.find({ id_auteur: { $in: (channel.users || []) } }).sort({
+        score: -1,
+        nom: 1
+      }).limit(3)
 
-    const [pp1, pp2, pp3] = await Promise.all(tmpUsers.map(user => getProfilePicture(user.id_auteur)))
+      const [pp1, pp2, pp3] = await Promise.all(tmpUsers.map(user => getProfilePicture(user.id_auteur)))
 
-    const { data } = await axios.post('http://localhost:11871/api/render', { html, content: { name1: tmpUsers[0].nom, pp1, name2: tmpUsers[1].nom, pp2, name3: tmpUsers[2].nom, pp3 }, width: 626, height: 352 }, { responseType: 'arraybuffer' })
-    const attachment = new MessageAttachment(Buffer.from(data), 'oui.png')
-    await interaction.reply({ files: [attachment] })
+      const { data } = await axios.post(process.env.API_RENDER || 'http://localhost:11871/api/render', {
+        html,
+        content: { name1: tmpUsers[0].nom, pp1, name2: tmpUsers[1].nom, pp2, name3: tmpUsers[2].nom, pp3 },
+        width: 626,
+        height: 352
+      }, { responseType: 'arraybuffer' })
+      const attachment = new MessageAttachment(Buffer.from(data), 'oui.png')
+      await interaction.reply({ files: [attachment] })
+    } catch (err) {
+      await interaction.reply({ content: 'Erreur désolé...', ephemeral: true })
+    }
   }
 }
