f57ce4e9fa4060a4b5be1cf3695ebeb3a5ecb3b9
kernoeb <kernoeb@protonmail.com>
2021-10-03 00:59:27 +0200

    feat: podium image (now with puppeteer)

---
+++ commands/podium.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageAttachment } = require('discord.js')
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+const { getProfilePicture } = require('../utils/get_profile_picture')
+const mongoose = require('../utils/mongoose')
+
+const html = fs.readFileSync(path.join(process.cwd(), '/assets/podium.html'), 'utf-8')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('podium')
+    .setDescription('Podium sous forme d\'image'),
+  async execute(interaction) {
+    const channel = await mongoose.models.channels.findOne({ guildId: interaction.guildId })
+    const tmpUsers = await mongoose.models.user.find({ id_auteur: { $in: (channel.users || []) } }).sort({ score: -1, nom: 1 }).limit(3)
+
+    const [pp1, pp2, pp3] = await Promise.all(tmpUsers.map(user => getProfilePicture(user.id_auteur)))
+
+    const { data } = await axios.post('http://localhost:11871/api/render', { html, content: { name1: tmpUsers[0].nom, pp1, name2: tmpUsers[1].nom, pp2, name3: tmpUsers[2].nom, pp3 }, width: 626, height: 352 }, { responseType: 'arraybuffer' })
+    const attachment = new MessageAttachment(Buffer.from(data), 'oui.png')
+    await interaction.reply({ files: [attachment] })
+  }
+}
--- utils/get_profile_picture.js
+++ utils/get_profile_picture.js
@@ -1,19 +1,34 @@
-const axios = require('../utils/axios')
+const axios = require('axios')
+const http = require('http')
+const https = require('https')
+
+const instance = axios.create({
+  baseURL: process.env.ROOTME_API_URL,
+  timeout: 3000,
+  httpAgent: new http.Agent({ keepAlive: true }),
+  httpsAgent: new https.Agent({ keepAlive: true })
+})
+
 module.exports = {
   async getProfilePicture(id) {
     try {
       const url = `${process.env.ROOTME_URL}/IMG/auton${id}.jpg`
-      await axios.get(url, { params: { fakeHash: new Date().getTime() } })
+      await instance.get(url, { params: { fakeHash: new Date().getTime() } })
       return url
     } catch (err) {}
     try {
       const url = `${process.env.ROOTME_URL}/IMG/auton${id}.png`
-      await axios.get(url, { params: { fakeHash: new Date().getTime() } })
+      await instance.get(url, { params: { fakeHash: new Date().getTime() } })
       return url
     } catch (err) {}
     try {
       const url = `${process.env.ROOTME_URL}/IMG/auton${id}.gif`
-      await axios.get(url, { params: { fakeHash: new Date().getTime() } })
+      await instance.get(url, { params: { fakeHash: new Date().getTime() } })
+      return url
+    } catch (err) {}
+    try {
+      const url = `${process.env.ROOTME_URL}/IMG/auton0.png`
+      await instance.get(url)
       return url
     } catch (err) {}
     return null
