ab135a2560f34896a3da199b457a6f0cbd7cb77d
kernoeb <kernoeb@protonmail.com>
2021-12-04 20:29:01 +0100

    Update chart.js

--- commands/chart.js
+++ commands/chart.js
@@ -5,15 +5,14 @@ const { getMonthChart } = require('../utils/charts')
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('chart')
-    .setDescription('Un maximum de statistiques.')
-    .addStringOption(option =>
+    .setDescription('Un maximum de statistiques.'), /* .addStringOption(option =>
       option.setName('type')
         .setDescription('Type de graphique')
-        .setRequired(true)),
+        .setRequired(true))*/
   async execute(interaction) {
     await interaction.deferReply()
 
-    const attachment = new MessageAttachment(await getMonthChart(interaction.guildId), `month-${interaction.guildId}.png`)
+    const attachment = new MessageAttachment(await getMonthChart(interaction.guildId))
     await interaction.editReply({ files: [attachment] })
   }
 }
