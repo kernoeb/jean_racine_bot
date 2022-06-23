288c5384037720713bc57ed57ce2ec9dfc08a5dd
Antabuse-does-something <64858450+Antabuse-does-something@users.noreply.github.com>
2022-06-23 18:37:39 -0400

    Code cleanup

--- commands/next_ctf.js
+++ commands/next_ctf.js
@@ -1,89 +1,73 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { curly } = require('node-libcurl');
-const {MessageEmbed} = require('discord.js');
-const logger = require('../utils/signale');
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { curly } = require('node-libcurl')
+const { MessageEmbed } = require('discord.js')
+const logger = require('../utils/signale')
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('nextctf')
-        .setDescription('Shows the Next CTF available in CTFTime')
-        .addIntegerOption(option => option.setName('numberctf').setDescription('Number of CTF to show')),
-    
-    async execute(interaction){
-        let nbCtf = interaction.options.getInteger('numberctf');
-        // if no arguments has been provided sets it to 1
-        if(!nbCtf){
-            nbCtf = 1;
-        }
-        // basic sanatize of te argument
-        else{
-            nbCtf = nbCtf > 5 ? 5 : nbCtf;
-            nbCtf = nbCtf < 1 ? 1 : nbCtf;
-        }
-        await interaction.reply("Fetching the CTF...");
+  data: new SlashCommandBuilder()
+    .setName('nextctf')
+    .setDescription('Shows the Next CTF available in CTFTime')
+    .addIntegerOption(option => option.setName('numberctf').setDescription('Number of CTF to show')),
 
-        curly.get(`https://ctftime.org/api/v1/events/?limit=${nbCtf}&start=${interaction.createdTimestamp}&finish=${interaction.createdTimestamp+604800000}`)
-            .then(
-                response =>{
-                    if(response.statusCode == 200){
-                        return response.data
-                    }
-                    else{
-                        throw logger.error("Error : CTFTime might be down");
-                    }
-                },
-                async err => {
-                    await interaction.editReply("Internal error");
-                    throw logger.error(err);
-                }
-            )
-            .then(
-                async body => {
-                    if(body === []){
-                        let embed = new MessageEmbed()
-                                .setTitle(`Error :x:`)
-                                .setColor("RED")
-                                .addField(`There is no CTF in the next 7 days`,`Please try again later`);
-                        await interaction.editReply({embeds : [embed]});
-                        return;
-                    }
-                    nbCtf = nbCtf > body.length ? body.length : nbCtf;
-                    let embedArray = [];
-                    for(let i = 0; i < nbCtf; i++){
-                        let start = body[i].start.split("T");
-                        let end = body[i].finish.split("T");
-                        let embed = new MessageEmbed()
-                        .setTitle("Upcomming CTFs")
-                        .setColor("#36393f");
-                        if (body[i].title == '_EVENT CHANGED_' && i <body.length -1) {
-                            embed.setDescription(body[i+1].title + "**event changed**",`${body[i+1].description}`);
-                            i++;
-                        }
-                        else{
-                            embed.setDescription(body[i].title,`${body[i].description}`);
-                        }        
-                        embed.addField(
-                            ":information_source: Infos",
-                            `**Starts on :** ${start[0]}, at : ${start[1]} \n
-                            **Host by :** ${body[i].organizers.name} \n
-                            **Ends :** ${end[0]}, at : ${end[1]} \n
-                            **Website :** ${body[i].url} \n
-                            **CTF Time URL :** ${body[i].ctftime_url} \n
-                            **IRL CTF ? :** ${body[i].onsite} \n
-                            **Format :** ${body[i].format} \n 
-                            **Duration :** ${body[i].duration.hours} Heures & ${body[i].duration.days} Jours \n 
-                            **Number of Teams Interested :** ${body[i].participants} \n
-                            **Weight** ${body[i].weight} \n
-                            **CTF ID :** ${body[i].id}`
-                        );
-                        embed.setThumbnail(body[i].logo);
-                        embedArray.push(embed);
-                    }
-                    await interaction.editReply({embeds : embedArray});
-                },
-                async err => {
-                    await interaction.editReply("Error : CTFTime might be down");
-                }
-            )
-    } 
+  async execute(interaction) {
+    let nbCtf = interaction.options.getInteger('numberctf')
+    // if no arguments has been provided sets it to 1
+    if(!nbCtf)
+      nbCtf = 1
+    // basic sanatize of te argument
+    else{
+      nbCtf = nbCtf > 5 ? 5 : nbCtf
+      nbCtf = nbCtf < 1 ? 1 : nbCtf
+    }
+    await interaction.reply('Fetching the CTF...')
+
+    let response = await curly.get(`https://ctftime.org/api/v1/events/?limit=${nbCtf}&start=${interaction.createdTimestamp}&finish=${interaction.createdTimestamp + 604800000}`)
+    if(response.statusCode == 200) {
+      response = response.data
+    } else{
+      logger.error('Error : CTFTime might be down')
+      return interaction.editReply({ content: 'Error while fetching the CTF', ephemeral: true })
+    }
+    const body = response
+    if(body === []) {
+      const embed = new MessageEmbed()
+        .setTitle('Error :x:')
+        .setColor('RED')
+        .addField('There is no CTF in the next 7 days', 'Please try again later')
+      await interaction.editReply({ embeds : [embed] })
+      return
+    }
+    nbCtf = nbCtf > body.length ? body.length : nbCtf
+    const embedArray = []
+    for(let i = 0; i < nbCtf; i++) {
+      const start = body[i].start.split('T')
+      const end = body[i].finish.split('T')
+      const embed = new MessageEmbed()
+        .setTitle('Upcomming CTFs')
+        .setColor('#36393f')
+      if (body[i].title == '_EVENT CHANGED_' && i < body.length - 1) {
+        embed.setDescription(body[i + 1].title + '**event changed**', `${body[i + 1].description}`)
+        i++
+      } else{
+        embed.setDescription(body[i].title, `${body[i].description}`)
+      }
+      embed.addField(
+        ':information_source: Infos',
+        `**Starts on :** ${start[0]}, at : ${start[1]} \n
+                **Host by :** ${body[i].organizers.name} \n
+                **Ends :** ${end[0]}, at : ${end[1]} \n
+                **Website :** ${body[i].url} \n
+                **CTF Time URL :** ${body[i].ctftime_url} \n
+                **IRL CTF ? :** ${body[i].onsite} \n
+                **Format :** ${body[i].format} \n 
+                **Duration :** ${body[i].duration.hours} Heures & ${body[i].duration.days} Jours \n 
+                **Number of Teams Interested :** ${body[i].participants} \n
+                **Weight** ${body[i].weight} \n
+                **CTF ID :** ${body[i].id}`
+      )
+      embed.setThumbnail(body[i].logo)
+      embedArray.push(embed)
+    }
+    await interaction.editReply({ embeds : embedArray })
+  }
 }
--- commands/pick_ctf.js
+++ commands/pick_ctf.js
@@ -1,85 +1,75 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { curly } = require('node-libcurl');
-const {MessageEmbed} = require('discord.js');
-const logger = require('../utils/signale');
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { curly } = require('node-libcurl')
+const { MessageEmbed } = require('discord.js')
+const logger = require('../utils/signale')
 
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('pickctf')
-        .setDescription('Choose a CTF from CTF time and create a pool to see who is interested in it')
-        .addIntegerOption(option => option.setName('id').setDescription('the id of the CTF').setRequired(true)),
-    async execute(interaction) {
-        //Checking for the permission
-        let id = interaction.options.getInteger('id');
-        if(!id){
-            return interaction.reply("Please specify a CTF id");
-        }
-        await interaction.deferReply();
-        curly.get(`https://ctftime.org/api/v1/events/${id}/`)
-            .then(
-                response => {
-                    if(response.statusCode == 200){
-                        return response.data
-                    }
-                    else{
-                        throw logger.error("Error while fetching the CTF");
-                    }
-                    
-                },
-                err => {
-                    throw error(err);
-                }
-            )
-            .then(
-                async body => {
-                    let start = body.start.split('T');
-                    let end = body.finish.split('T');
-                    let embed = new MessageEmbed()
-                        .setTitle("Poll for the next CTF")
-                        .setDescription(body.title,`${body.description}`)
-                        .setColor('#36393f')
-                        .addField(
-                            ":information_source: Infos",
-                            `**Starts on :** ${start[0]}, at : ${start[1]} \n
-                            **Host by :** ${body.organizers.name} \n
-                            **Ends :** ${end[0]}, at : ${end[1]} \n
-                            **Website :** ${body.url} \n
-                            **CTF Time URL :** ${body.ctftime_url} \n
-                            **IRL CTF ? :** ${body.onsite} \n
-                            **Format :** ${body.format} \n 
-                            **Duration :** ${body.duration.hours} Heures & ${body.duration.days} Jours \n 
-                            **Number of Teams Interested :** ${body.participants} \n
-                            **Weight** ${body.weight} \n
-                            **CTF ID :** ${body.id}`
-                        )
-                        .setThumbnail(body.logo);
-                    await interaction.editReply({embeds: [embed]});
-                    const msg = await interaction.fetchReply();
-                    msg.react('✅').then(() => msg.react('❌'));
-                    const reactionArray = ['✅', '❌'];
-                    async function vote(){
-                        interaction.fetchReply().then(async msg => {
-                            let count = [];
-                            for(let i = 0; i < reactionArray.length; i++){
-                                count[i] = msg.reactions.cache.get(reactionArray[i]).count -1; // Removes the bot's vote
-                            }
-                            let nbVote = count[0] + count[1];
-                            let embed = new MessageEmbed()
-                                .setTitle("Results of the poll")
-                                .setDescription(`The poll has ended, the results are :`)
-                                .addField("Stats : ",`✅ : ${100 * count[0] / nbVote}% \n ❌ : ${100 * count[1] / nbVote}% \n Number of votes : ${nbVote}`)
-                                .setThumbnail(body.logo);
-                            await interaction.followUp({embeds: [embed]});
-                        })
-                    }
-                    setTimeout(vote,24 * 60 * 60 * 1000);
-                },
-                async err => {
-                    let content = "Error: The id " + id +" is incorect or CTFTime is down";
-                    await interaction.editReply(content);
-                    logger.error(content);
-                }
-            )
+  data: new SlashCommandBuilder()
+    .setName('pickctf')
+    .setDescription('Choose a CTF from CTF time and create a pool to see who is interested in it')
+    .addIntegerOption(option => option.setName('id').setDescription('the id of the CTF').setRequired(true)),
+  async execute(interaction) {
+    // Checking for the permission
+    const id = interaction.options.getInteger('id')
+    if(!id) {
+      return interaction.reply('Please specify a CTF id')
     }
-};
+    await interaction.deferReply()
+    let response = await curly.get(`https://ctftime.org/api/v1/events/${id}/`)
+    if(response.statusCode == 200) {
+      response = response.data
+    } else{
+      logger.error('Error while fetching the CTF')
+      return interaction.editReply({ content: 'Error while fetching the CTF', ephemeral: true })
+    }
+    const body = response
+    // Prepares the message
+    const start = body.start.split('T')
+    const end = body.finish.split('T')
+    const embed = new MessageEmbed()
+      .setTitle('Poll for the next CTF')
+      .setDescription(body.title, `${body.description}`)
+      .setColor('#36393f')
+      .addField(
+        ':information_source: Infos',
+        `**Starts on :** ${start[0]}, at : ${start[1]} \n
+                **Host by :** ${body.organizers.name} \n
+                **Ends :** ${end[0]}, at : ${end[1]} \n
+                **Website :** ${body.url} \n
+                **CTF Time URL :** ${body.ctftime_url} \n
+                **IRL CTF ? :** ${body.onsite} \n
+                **Format :** ${body.format} \n 
+                **Duration :** ${body.duration.hours} Heures & ${body.duration.days} Jours \n 
+                **Number of Teams Interested :** ${body.participants} \n
+                **Weight** ${body.weight} \n
+                **CTF ID :** ${body.id}`
+      )
+      .setThumbnail(body.logo)
+    // Sends the message
+    await interaction.editReply({ embeds: [embed] })
+    // Create the vote
+    const msg = await interaction.fetchReply()
+    msg.react('✅').then(() => msg.react('❌'))
+    const reactionArray = ['✅', '❌']
+
+    // Fetch the message to get the result
+    async function vote() {
+      interaction.fetchReply().then(async message => {
+        const count = []
+        for(let i = 0; i < reactionArray.length; i++) {
+          count[i] = message.reactions.cache.get(reactionArray[i]).count - 1 // Removes the bot's vote
+        }
+        const nbVote = count[0] + count[1]
+        const resultEmbed = new MessageEmbed()
+          .setTitle('Results of the poll')
+          .setDescription('The poll has ended, the results are :')
+          .addField('Stats : ', `✅ : ${100 * count[0] / nbVote}% \n ❌ : ${100 * count[1] / nbVote}% \n Number of votes : ${nbVote}`)
+          .setThumbnail(body.logo)
+        await interaction.followUp({ embeds: [resultEmbed] })
+      })
+    }
+    // Timer for the vote
+    setTimeout(vote, 24 * 60 * 60 * 1000)
+  }
+}
