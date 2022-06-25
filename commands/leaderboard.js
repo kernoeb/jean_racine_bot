f19e67266fd9839647a7524339e1255c08724509
Antabuse-does-something <64858450+Antabuse-does-something@users.noreply.github.com>
2022-06-25 09:34:54 -0400

    refactor leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -2,6 +2,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders')
 const { curly } = require('node-libcurl')
 const { MessageEmbed } = require('discord.js')
 const logger = require('signale-logger')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,6 +16,11 @@ module.exports = {
     let locale = interaction.options.getString('locale')
     // Defer the reply to permit more time to execute the command
     await interaction.deferReply()
+    // Declare the embed globally to avoid code redundancy
+    const embed = new MessageEmbed()
+      .setTitle('Top 10 global de CTFTime')
+      .setThumbnail('https://avatars.githubusercontent.com/u/2167643?s=200&v=4')
+      .setURL('https://ctftime.org/')
     // Checks if the user provided a locale if not shows the general top 10
     if (!locale) {
       let request = await curly.get('https://ctftime.org/api/v1/top/')
@@ -25,14 +32,9 @@ module.exports = {
         logger.error('Error while fetching the TOP 10 CTFTime might be down')
         return interaction.editReply({ content: 'Erreur CTFTime est peut être down', ephemeral: true })
       }
-
-      const embed = new MessageEmbed()
-        .setTitle('Top 10 global de CTFTime')
-        .setThumbnail('https://avatars.githubusercontent.com/u/2167643?s=200&v=4')
-        .setURL('https://ctftime.org/')
       request = Object.values(request)[0]
       for (let i = 0; i < request.length; i++) {
-        embed.addField(request[i].team_name, `Place : ${i}`)
+        embed.addField(request[i].team_name, `Place : ${i + 1}`)
       }
       return await interaction.editReply({ embeds: [embed] })
     }
@@ -54,67 +56,12 @@ module.exports = {
       })
 
     }
-    // Get the top 10 teams
-    const text = response
-    const txtparse = text.split('\n')
-    const top10 = []
-    let i = 0
-    for (const string of txtparse) {
-      if (string.includes('<tr><td class="place">') && i < 10) {
-        let str = ''
-        str += string
-        top10.push(str.trim())
-        i++
-      }
-    }
-    // Get the TOP 10 teams ids
-    const ids = []
-    for (let j = 0; j < top10.length; j++) {
-      for (let k = 0; k < top10[j].length; k++) {
-        if (top10[j][k] <= '9' && top10[j][k] >= '0' && top10[j][k - 1] === '/') {
-          let id = '' + top10[j][k]
-          while (top10[j][k + 1] !== '"') {
-            k++
-            id += top10[j][k]
-          }
-          ids.push([id, j + 1])
-        }
-      }
-    }
-
-    // construct the embed
-    const msgtop10 = new MessageEmbed()
-    msgtop10.setTitle('TOP 10 CTFTime du pays ' + locale)
-    msgtop10.setThumbnail('https://avatars.githubusercontent.com/u/2167643?s=200&v=4')
-    msgtop10.setURL('https://ctftime.org/')
-    interaction.editReply({ embeds: [msgtop10] })
-    let x = 0
-
-    // TODO make this better, or get more info :)
-    function master() {
-      // Set an interval to avoid the rate limit
-      const inter = setInterval(messageSender, 1500)
-
-      async function messageSender() {
-        try {
-          if (x < 10) {
-            const [id, place] = ids[x]
-            x++
-            // get the team name via the CTF time API
-            const resp = await curly.get(`https://ctftime.org/api/v1/teams/${id}/`)
-            const json = resp.data
-            msgtop10.addField(json.name, 'Place : ' + place)
-            await interaction.editReply({ embeds: [msgtop10] })
-          } else {
-            clearInterval(inter)
-          }
-        } catch (e) {
-          clearInterval(inter)
-          logger.error('Error while fetching the TOP10 CTFTime might be down')
-        }
-      }
-    }
-
-    master()
+    const dom = new JSDOM(response)
+    const matches = [...dom.window?.document?.querySelector('tbody')?.textContent?.matchAll(/\d+([a-zA-Z ]+)\d+/g)]
+    let counter = 1
+    matches.slice(0, 10).forEach(match => {
+      embed.addField(match[1], `Place ${counter++}`)
+    })
+    await interaction.editReply({ embeds : [embed] })
   }
 }
