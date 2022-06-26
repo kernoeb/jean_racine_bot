357f3ede4408403d00db7d60ad907c79413b954b
kernoeb <kernoeb@protonmail.com>
2022-06-26 22:25:04 +0200

    wip: podium with puppeteer

--- commands/podium.js
+++ commands/podium.js
@@ -1,12 +1,12 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-// const { MessageAttachment } = require('discord.js')
-// const { curly } = require('node-libcurl')
-// const logger = require('../utils/signale')
-// const fs = require('fs')
-// const path = require('path')
-// const { getProfilePicture } = require('../utils/get_profile_picture')
-// const mongoose = require('../utils/mongoose')
-// const html = fs.readFileSync(path.join(process.cwd(), '/assets/podium.html'), 'utf-8')
+const { MessageAttachment } = require('discord.js')
+const { curly } = require('node-libcurl')
+const logger = require('../utils/signale')
+const fs = require('fs')
+const path = require('path')
+const { getProfilePicture } = require('../utils/get_profile_picture')
+const mongoose = require('../utils/mongoose')
+const html = fs.readFileSync(path.join(process.cwd(), '/assets/podium.html'), 'utf-8')
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,9 +14,9 @@ module.exports = {
     .setDescription('Podium sous forme d\'image'),
   async execute(interaction) {
     await interaction.deferReply()
-    return await interaction.editReply({ content: 'Commande en maintenance, elle revient plus tard !' })
+    // return await interaction.editReply({ content: 'Commande en maintenance, elle revient plus tard !' })
 
-    /* try {
+    try {
       const channel = await mongoose.models.channels.findOne({ guildId: interaction.guildId })
       const tmpUsers = await mongoose.models.user.find({ id_auteur: { $in: (channel.users || []) } }).sort({
         score: -1,
@@ -47,6 +47,6 @@ module.exports = {
     } catch (err) {
       logger.error(err)
       await interaction.editReply({ content: 'Erreur désolé...', ephemeral: true })
-    }*/
+    }
   }
 }
